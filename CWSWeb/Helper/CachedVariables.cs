2b5559c787e391f37ae2779d67df3acd9066c931
ChrisK91 <ChrisGit@outlook.com>
2013-07-22 20:40:52 +0200

    Additional bugfixes

--- CWSWeb/Helper/CachedVariables.cs
+++ CWSWeb/Helper/CachedVariables.cs
@@ -153,37 +153,45 @@ namespace CWSWeb.Helper
             public static string ReadEndTokens(string path, Int64 numberOfTokens, Encoding encoding, string tokenSeparator)
             {
 
-                int sizeOfChar = encoding.GetByteCount("\n");
-                byte[] buffer = encoding.GetBytes(tokenSeparator);
-
-
-                using (FileStream fs = new FileStream(path, FileMode.Open))
+                try
                 {
-                    Int64 tokenCount = 0;
-                    Int64 endPosition = fs.Length / sizeOfChar;
+                    int sizeOfChar = encoding.GetByteCount("\n");
+                    byte[] buffer = encoding.GetBytes(tokenSeparator);
 
-                    for (Int64 position = sizeOfChar; position < endPosition; position += sizeOfChar)
+
+                    using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
                     {
-                        fs.Seek(-position, SeekOrigin.End);
-                        fs.Read(buffer, 0, buffer.Length);
+                        Int64 tokenCount = 0;
+                        Int64 endPosition = fs.Length / sizeOfChar;
 
-                        if (encoding.GetString(buffer) == tokenSeparator)
+                        for (Int64 position = sizeOfChar; position < endPosition; position += sizeOfChar)
                         {
-                            tokenCount++;
-                            if (tokenCount == numberOfTokens)
+                            fs.Seek(-position, SeekOrigin.End);
+                            fs.Read(buffer, 0, buffer.Length);
+
+                            if (encoding.GetString(buffer) == tokenSeparator)
                             {
-                                byte[] returnBuffer = new byte[fs.Length - fs.Position];
-                                fs.Read(returnBuffer, 0, returnBuffer.Length);
-                                return encoding.GetString(returnBuffer);
+                                tokenCount++;
+                                if (tokenCount == numberOfTokens)
+                                {
+                                    byte[] returnBuffer = new byte[fs.Length - fs.Position];
+                                    fs.Read(returnBuffer, 0, returnBuffer.Length);
+                                    return encoding.GetString(returnBuffer);
+                                }
                             }
                         }
-                    }
 
-                    // handle case where number of tokens in file is less than numberOfTokens
-                    fs.Seek(0, SeekOrigin.Begin);
-                    buffer = new byte[fs.Length];
-                    fs.Read(buffer, 0, buffer.Length);
-                    return encoding.GetString(buffer);
+                        // handle case where number of tokens in file is less than numberOfTokens
+                        fs.Seek(0, SeekOrigin.Begin);
+                        buffer = new byte[fs.Length];
+                        fs.Read(buffer, 0, buffer.Length);
+                        return encoding.GetString(buffer);
+                    }
+                }
+                catch (IOException)
+                {
+                    //File in use
+                    return "";
                 }
             }
         }
