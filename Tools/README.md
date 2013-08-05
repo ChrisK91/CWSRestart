20dc123fcde5fa40192c6ee28376c10c271f5b6e
ChrisK91 <ChrisGit@outlook.com>
2013-08-05 12:03:26 +0200

    Setup Script
    Added a PowerShell script to unblock downloaded files.

---
+++ Tools/README.md
@@ -0,0 +1,5 @@
+# Tools for CWSRestart
+Here you can find some tools, to help you get started with CWSRestart
+
+## Setup PowerShell Script
+You can use this script, to unblock downloaded files and add a Windows-Firewall setting for port 8181. Download the file and place it in either the CWSRestart, CWSWeb or MITM folder. Then `cd` to the location, where this script is stored in a PowerShell console (I recommend launching PS as Administrator right away). Then you can start the script with `.\Setup.ps1`.
---
+++ Tools/Setup.ps1
@@ -0,0 +1,79 @@
+﻿function Test-Administrator  
+{  
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
+}
+
+$dir = Get-Location
+$cwsFiles = (
+    "CubeWorldMITM.exe",
+    "Ionic.Zip.dll",
+    "CWSProtocol.dll",
+    "ServerService.dll",
+    "System.Data.SQLite",
+    "SQLite.Interop.dll",
+    "CWSRestart.exe",
+    "Interop.NATUPNPLib.dll",
+    "CWSWeb.exe",
+    "Nancy.Authentication.Forms.dll",
+    "Nancy.dll",
+    "Nancy.Hosting.Self.dll",
+    "Nancy.ViewEngines.Razor",
+    "System.Web.Razor.Unofficial.dll")
+$cwsWebName = "CWSWeb.exe"
+
+"Current directory: " + $dir
+
+$selection = "";
+
+while("y", "n" -notcontains $selection.ToLowerInvariant())
+{
+    $selection = Read-Host "Would you like to unblock our files in the current directory (y/n)?"
+}
+
+if($selection.ToLowerInvariant() -eq "y")
+{
+    "Unblocking files"
+    foreach($f in gci $dir -Recurse -File | ForEach-Object -Process {$_.FullName})
+    {
+        foreach($n in $cwsFiles)
+        {
+            if($f.Contains($n))
+            {
+                "Unblocking file " + $f
+                Unblock-File $f
+            }
+        }
+    }
+}
+
+if(Test-Administrator)
+{
+    $selection = "";
+
+    while("y", "n" -notcontains $selection.ToLowerInvariant())
+    {
+        $selection = Read-Host "Would you like to add a Windows Firewall Rule for Port 8181 (y/n)?"
+    }
+
+    if($selection.ToLowerInvariant() -eq "y")
+    {
+        if(Test-Path($dir.FullName + $cwsWebName))
+        {
+            $programPath = $dir.FullName + $cwsWebName
+
+            New-NetFirewallRule -DisplayName "CWSWeb 8181 Inbound TCP" -Program $programPath -Direction Inbound -RemotePort 8181 -LocalPort 8181 -Profile Any -Action Allow -Protocol TCP
+            New-NetFirewallRule -DisplayName "CWSWeb 8181 Outbound TCP" -Program $programPath -Direction Outbound -RemotePort 8181 -LocalPort 8181 -Profile Any -Action Allow -Protocol TCP
+            New-NetFirewallRule -DisplayName "CWSWeb 8181 Inbound UDP" -Program $programPath -Direction Inbound -RemotePort 8181 -LocalPort 8181 -Profile Any -Action Allow -Protocol UDP
+            New-NetFirewallRule -DisplayName "CWSWeb 8181 Outbound UDP" -Program $programPath -Direction Outbound -RemotePort 8181 -LocalPort 8181 -Profile Any -Action Allow -Protocol UDP
+        }
+        else
+        {
+            Write-Warning "CWSWeb was not found in the current directory"
+        }
+    }
+}
+else
+{
+    "Run this script as administrator, to add Firewall rules"
+}
