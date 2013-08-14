7992c7ad3ec0bcd31250d06507c50603e22d8aac
ChrisK91 <ChrisGit@outlook.com>
2013-08-15 00:39:51 +0200

    Added generation script for long ip lists

---
+++ Tools/GenerateSampleIPFile.ps1
@@ -0,0 +1,49 @@
+﻿#This file generates an iplist with the specified number of IPs
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+
+$number = 10000;
+
+$file = $scriptPath + "\iplist.txt";
+
+$content = "";
+
+$a = 128;
+$b = 0;
+$c = 0;
+$d = 0;
+
+for($i = 0; $i -le $number; $i++)
+{
+    $ip = "{0}.{1}.{2}.{3}" -f $a, $b, $c, $d; 
+
+    $d = $d + 1;
+
+    if($d -gt 255)
+    {
+        $d = 0;
+        $c = $c + 1;
+    }
+
+    if($c -gt 255)
+    {
+        $c = 0;
+        $b = $b + 1;
+    }
+
+    if($b -gt 255)
+    {
+        $b = 0;
+        $a = $a + 1;
+    }
+
+    $content += "{0}`r`n" -f $ip;
+
+    $percentage = ($i / $number) * 100;
+
+    Write-Progress -Activity "Generating IPs" -Status "Percent generated:" -PercentComplete $percentage;
+}
+Write-Host "Creating file";
+
+$content | Out-File $file -Force;
+
+Write-Host "Done";
--- Tools/README.md
+++ Tools/README.md
@@ -3,3 +3,6 @@ Here you can find some tools, to help you get started with CWSRestart
 
 ## Setup PowerShell Script
 You can use this script, to unblock downloaded files and add a Windows-Firewall setting for port 8181. Download the file and place it in either the CWSRestart, CWSWeb or MITM folder. Then `cd` to the location, where this script is stored in a PowerShell console (I recommend launching PS as Administrator right away). Then you can start the script with `.\Setup.ps1`.
+
+## iplist.txt generation script
+Creates an iplist.txt for CWSRestart for testing purposes.
